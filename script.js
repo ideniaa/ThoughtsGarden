940c1ecb8421efb1191e855e6f0fdbfbebd3d4c4
Paulene Mendoza Morrison <paulene.doza@gmail.com>
2025-04-18 23:48:42 -0700

    Update 1 script.js

--- script.js
+++ script.js
@@ -16,3 +16,30 @@ form.addEventListener('submit', function (e) {
 });
 
 
+
+// TO change background based on emotion, FIX FIX FIX 
+/*
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  // When emotion = happy, change sunny.png
+  const happyButton = document.querySelector('.happy');
+  happyButton.addEventListener('click', function() {
+    document.body.style.backgroundImage = "url('./images/sunny.jpg')";
+  });
+
+  // When emotion = sad, change rainy.png
+  const sadButton = document.querySelector('.sad');
+  sadButton.addEventListener('click', function() {
+    document.body.style.backgroundImage = "url('./images/rainy.jpg')"; // Change to rainy background
+  });
+
+  // When angry = happy, change stormy.png
+  const angryButton = document.querySelector('.angry');
+  angryButton.addEventListener('click', function() {
+    document.body.style.backgroundImage = "url('./images/stormy.jpg')"; // Change to stormy background
+  });
+});
+
+*/
+
